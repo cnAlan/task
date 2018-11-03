ecc27a5e96b8719820db9604ad258855367f85a3
Alan <2522658159@qq.com>
2018-11-03 21:35:10 +0800

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-var people = {testc: {test: 10, testtime: 110}, testa: {test: 10, testtime: 110}, testb: {test: 10, testtime: 110}}
-
-console.table(people);
-
-console.log(parseInt(process.version.split('.')[0].substr(1)) >= 10)
