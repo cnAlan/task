bb73f05fe5c745ac42ee7b1833277dec2e04a490
Alan <2522658159@qq.com>
2018-11-03 21:35:20 +0800

    Delete test_http.js

--- test_http.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-const server = http.createServer((req, res)=> {
-    res.end('HelloMyCommandLineTools!')
-})
-exports.listen = port=> server.listen(port, function(err) {
-    console.log(err)
-})
-module.export = http
